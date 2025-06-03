e2e18d781c3ec25483f1ad4d3556ea71c0fee4b5
RahmaFahrurrozi <zfahrur150105@gmail.com>
2025-06-03 10:04:03 +0700

    chore: adding shadcn-ui-library

---
+++ frontend/src/pages/home/index.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const HomePage = () => {
+  return (
+    <div className="flex justify-center items-center flex-col gap-2 align-middle">
+      <div>Home Page</div>
+      <Button variant={"default"}>Isi Ja sendiri</Button>
+    </div>
+  );
+};
+
+export default HomePage;
