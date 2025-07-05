7122b5faf03ec29c939c79c8342f05f9b133d6c1
RahmaFahrurrozi <zfahrur150105@gmail.com>
2025-07-05 09:08:20 +0700

    feat: add loading state to login form submission handling

--- frontend/src/pages/auth/Login/hooks/useLoginForm.ts
+++ frontend/src/pages/auth/Login/hooks/useLoginForm.ts
@@ -2,8 +2,11 @@ import { useForm } from "react-hook-form";
 import type { loginSchemaType } from "../schema/login-schema";
 import loginSchema from "../schema/login-schema";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { toast } from "sonner";
+import { useState } from "react";
 
 const useLoginFrom = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const loginForm = useForm<loginSchemaType>({
     resolver: zodResolver(loginSchema),
     defaultValues: {
@@ -12,13 +15,30 @@ const useLoginFrom = () => {
     },
   });
 
-  const handleSubmit = (values: loginSchemaType) => {
+  const handleSubmit = async (values: loginSchemaType) => {
+    setIsLoading(true);
+
     console.log("Form submitted with data:", values);
+
+    if (loginForm.formState.isValid) {
+      // Handle form submission
+      console.log("Login successful!");
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+      setIsLoading(false);
+      toast.success("Login successful!");
+    } else {
+      // Handle form validation errors
+      console.log("Form validation errors:", loginForm.formState.errors);
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+      setIsLoading(false);
+      toast.error("Failed to login!");
+    }
   };
 
   return {
     loginForm,
     handleSubmit,
+    isLoading,
   };
 };
 
