62a8f8e0438e69c68d102b18c15c17bba8810a35
RahmaFahrurrozi <zfahrur150105@gmail.com>
2025-07-05 09:08:38 +0700

    feat: add RegisterPage component with AuthLayout and RegisterForm

---
+++ frontend/src/pages/auth/Register/index.tsx
@@ -0,0 +1,12 @@
+import AuthLayout from "../layout/authLayout";
+import RegisterForm from "./components/register-form";
+
+const RegisterPage = () => {
+  return (
+    <AuthLayout>
+      <RegisterForm />
+    </AuthLayout>
+  );
+};
+
+export default RegisterPage;
